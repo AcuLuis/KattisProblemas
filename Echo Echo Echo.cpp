78f8516a1c00a26ba8b9276ee331befc0d99de64
AcuLuis <21101076@ue.edu.pe>
2025-02-06 23:06:27 -0500

    Echo Echo Echo

---
+++ Echo Echo Echo.cpp
@@ -0,0 +1,14 @@
+#include<string.h>
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    string eco;
+    int i=0;
+    cin>>eco;
+
+    while(i<3){
+        cout<<eco<<" ";
+        i++;
+    }
+}
