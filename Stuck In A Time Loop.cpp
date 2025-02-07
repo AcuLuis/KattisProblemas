2b466b06d80283c20554d99819046087c8b35c65
AcuLuis <21101076@ue.edu.pe>
2025-02-07 14:29:37 -0500

    Resolucion Stuck In A Time Loop

---
+++ Stuck In A Time Loop.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+int main(){
+    int n=0;
+    cin>>n;
+    if(n>=1 && n<=100){
+        for(int i=0;i<n;i++){
+            cout<<i+1<<" Abracadabra"<<endl;
+        }
+    }
+
+}
