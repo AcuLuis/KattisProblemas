40fa544cb77713fe0675b7cd42ec7e0a550c340e
AcuLuis <21101076@ue.edu.pe>
2025-02-06 23:33:37 -0500

    Resolucion Two-Sum

---
+++ Two-Sum.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+
+    int a=0, b=0;
+    cin>>a>>b;
+
+    if((a>=0 && a<=1000)&&(b>=0 && b<=1000)){
+        int suma = a+b;
+        cout<<suma<<endl;
+    }
+
+}
