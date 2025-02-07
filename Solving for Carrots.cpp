6532d5e365cef67faa510e9d13a00598ae331cfb
AcuLuis <21101076@ue.edu.pe>
2025-02-06 23:01:40 -0500

    Resolucion Solving for carrots

---
+++ Solving for Carrots.cpp
@@ -0,0 +1,18 @@
+#include<string.h>
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+    int concur=0, probl=0;
+    cin>>concur>>probl;
+
+    if((concur>=1 && concur<=1000) && (probl>=1 && probl<=1000)){
+
+        for(int i=0;i<concur;i++){
+            string aux="";
+            cin>>aux;
+        }
+        cout<<probl<<endl;
+    }
+}
