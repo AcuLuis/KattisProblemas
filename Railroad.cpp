a98c9b83d45db91e52450432f40e1da0f385ad7c
AcuLuis <21101076@ue.edu.pe>
2025-02-08 17:56:03 -0500

    Resolucion Railroad

---
+++ Railroad.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int x=0, y=0;
+    cin>>x>>y;
+    if(y%2!=0){
+        cout<<"impossible"<<endl;
+    }else{
+        cout<<"possible"<<endl;
+    }
+    return 0;
+}
